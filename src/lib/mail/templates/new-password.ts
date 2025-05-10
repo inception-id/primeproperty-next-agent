6be19e419a4b2948dc50b6fc774ee8f15c2f08ab
Stanley Winata <winatastanley355@gmail.com>
2025-05-10 15:46:46 +0700

    Email template update

--- src/lib/mail/templates/new-password.ts
+++ src/lib/mail/templates/new-password.ts
@@ -5,107 +5,87 @@ export const createNewPasswordTemplate = (resetPasswordLink: string) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Primepro Agent: New Password</title>
+      <title>Primepro Agent</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet"
+      />
     </head>
-    <body style="margin: 0; padding: 0; background-color: #f4f4f4">
-      <table
-        role="presentation"
-        width="100%"
-        cellpadding="0"
-        cellspacing="0"
-        style="background-color: #f4f4f4; padding: 20px 0"
+    <body
+      style="
+        margin: 0;
+        padding: 0;
+        font-family: &quot;PT Sans&quot;, sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        padding: 0.5rem;
+      "
+    >
+      <!-- TODO: Change link and image to Primepro -->
+      <div
+        style="
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          padding: 1rem 0;
+        "
       >
-        <tr>
-          <td align="center">
-            <table
-              role="presentation"
-              width="600"
-              cellpadding="0"
-              cellspacing="0"
-              style="
-                background-color: #ffffff;
-                border-radius: 8px;
-                overflow: hidden;
-              "
-            >
-              <!-- Header -->
-              <tr>
-                <td
-                  align="center"
-                  style="padding: 20px; background-color: #2a9df4"
-                >
-                  <a href="https://inception.id" target="_blank">
-                    <img
-                      src="https://inception.id/images/inception.png"
-                      width="120"
-                      alt="Primepro Logo"
-                      style="display: block; border: 0"
-                    />
-                  </a>
-                </td>
-              </tr>
-              <!-- Body -->
-              <tr>
-                <td
-                  style="
-                    padding: 30px;
-                    font-family: Arial, sans-serif;
-                    color: #333333;
-                  "
-                >
-                  <h1
-                    style="margin: 0 0 20px; font-size: 24px; font-weight: normal"
-                  >
-                    Reset Your Password
-                  </h1>
-                  <p style="margin: 0 0 20px; line-height: 1.5">
-                    Hi,<br /><br />
-                    Congratulations, your Primepro Agent account is ready to use.
-                    Click the button below to create new password:
-                  </p>
-                  <p style="text-align: center; margin: 30px 0">
-                    <a
-                      href="${resetPasswordLink}"
-                      role="button"
-                      aria-label="Reset Your Password"
-                      style="
-                        background-color: #2a9df4;
-                        color: #ffffff;
-                        text-decoration: none;
-                        padding: 15px 25px;
-                        border-radius: 4px;
-                        display: inline-block;
-                        font-size: 16px;
-                      "
-                    >
-                      Reset Password
-                    </a>
-                  </p>
-                  <p
-                    style="
-                      margin: 0 0 20px;
-                      line-height: 1.5;
-                      font-size: 12px;
-                      color: #777777;
-                    "
-                  >
-                    If the button doesn’t work, copy and paste this link into your
-                    browser:<br />
-                    <a href="${resetPasswordLink}" style="color: #2a9df4">${resetPasswordLink}</a>
-                  </p>
-                  <p style="margin: 0; font-size: 12px; color: #777777">
-                    This link will expire in 1 hour. If you have issue creating new
-                    password, please contact
-                    <a href="mailto:abby@primeproagent.com" style="color: #2a9df4"
-                      >abby@primeproagent.com</a
-                    >.
-                  </p>
-                </td>
-              </tr>
-            </table>
-          </td>
-        </tr>
-      </table>
+        <a href="https://inception.id" target="_blank">
+          <img
+            src="https://inception.id/images/inception.png"
+            width="100"
+            height="100"
+            alt="Primepro Logo"
+          />
+        </a>
+      </div>
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          max-width: 1024px;
+          margin: 0 auto;
+        "
+      >
+        <h1 style="margin-bottom: 0">Create Password</h1>
+        <p>
+          Congratulations, your Primepro Agent account is ready to use. Click the
+          button below to create new password:
+        </p>
+        <a
+          href="${resetPasswordLink}"
+          role="button"
+          style="
+            background-color: #ffff00;
+            color: #000000;
+            text-decoration: none;
+            padding: 1rem 1.5rem;
+            border-radius: 0.5rem;
+            margin-bottom: 1rem;
+            font-size: 20px;
+            font-weight: 700;
+            width: fit-content;
+          "
+        >
+          Create New Password
+        </a>
+        <p style="font-size: 14px">
+          If the button doesn’t work, copy and paste this link into your browser:
+          <br />
+          <a href="${resetPasswordLink}" style="font-size: 14px"
+            >${resetPasswordLink}</a
+          >
+        </p>
+        <p style="font-size: 14px">
+          This link will expire in 1 hour. If you have issue creating new
+          password, please contact
+          <a href="mailto:abby@primeproagent.com">abby@primeproagent.com</a>
+        </p>
+      </div>
     </body>
   </html>
   `;
